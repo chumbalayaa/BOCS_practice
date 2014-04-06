0969b409498862ba2f2cf899a7797bd1b8c6e01d
Alex Chumbley <achumbley@domU-12-31-39-0C-5C-F4.compute-1.internal>
2014-04-06 23:39:01 +0000

    Added new page

--- bocs/views.py
+++ bocs/views.py
@@ -1,6 +1,11 @@
 from flask import render_template, url_for, redirect
 from bocs import app
+import random
+
+def random_num():
+  return random.randint(1, 100)
+
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html', first=random_num(), second=random_num())
